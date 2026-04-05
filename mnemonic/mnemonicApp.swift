6ed13736d0a23b6c66a703d4544ae9de4c022fb8
Tristan Brewster <tristan@ir0k.com>
2026-04-04 23:02:54 -0400

    Initial Commit

---
+++ mnemonic/mnemonicApp.swift
@@ -0,0 +1,17 @@
+//
+//  mnemonicApp.swift
+//  mnemonic
+//
+//  Created by Tristan on 4/4/26.
+//
+
+import SwiftUI
+
+@main
+struct mnemonicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
